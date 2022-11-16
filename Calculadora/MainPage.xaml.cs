ff9d1cdc1372cdeb6532ba081e578690a13330c2
vmartinez@contacta.mx <vmartinez@contacta.mx>
2022-11-16 11:10:52 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora/MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_sumar(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_restar(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_por(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_dividir(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_one(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_two(Object sender, EventArgs e)
+        {
+
+        }
+        private void Click_three(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_four(Object sender, EventArgs e)
+        {
+
+        }
+        private void Click_five(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_six(Object sender, EventArgs e)
+        {
+
+        }
+        private void Click_seven(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_eight(Object sender, EventArgs e)
+        {
+
+        }
+        private void Click_nine(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_zero(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_equals(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_AC(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_C(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_return(Object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_point(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
